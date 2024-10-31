b20f9f4dd940f18371557d5d59d448ba1ccb2889
AgitDev-sudo <a.tunc1@student.han.nl>
2024-10-31 22:01:23 +0100

    A Example where a publisher is  added to the service example, we publish a counter

--- ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
+++ ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
@@ -5,6 +5,7 @@
 #include <rclc/executor.h>
 
 #include <my_custom_led_interface/srv/my_custom_led_control.h>
+#include <std_msgs/msg/int32.h>
 
 // Define pins for red and yellow LEDs
 #define RED_LED_PIN 3
@@ -14,10 +15,13 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}} 
 
-// Node and Service Handles
+// Node, Publisher, and Service Handles
 rcl_node_t node;
+rcl_publisher_t led_status_publisher;
+rcl_publisher_t counter_publisher; // Publisher voor de counter
 rcl_service_t led_control_service;
 rcl_timer_t led_status_timer;
+rcl_timer_t counter_timer;
 rclc_executor_t executor;
 
 rcl_allocator_t allocator;
@@ -26,12 +30,14 @@ rclc_support_t support;
 // Request and Response message allocations
 my_custom_led_interface__srv__MyCustomLedControl_Request led_request_msg;
 my_custom_led_interface__srv__MyCustomLedControl_Response led_response_msg;
+std_msgs__msg__Int32 counter_msg;
 
 // Status variables for the LEDs
-bool red_led_status = false;
-bool yellow_led_status = false;
+volatile bool red_led_status = false;
+volatile bool yellow_led_status = false;
+//int counter_value = 0; // Counter waarde die elke seconde wordt opgehoogd
 
-// Error handle loop, prefents for now that the esp won't reboot
+// Error handle loop, prevents for now that the esp won't reboot
 void error_loop()
 {
     while (1)
@@ -69,6 +75,17 @@ void led_control_callback(const void *req, void *res)
     response->message.size = strlen(response->message.data);
 }
 
+// Callback function for counter timer
+void counter_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
+{
+    RCLC_UNUSED(last_call_time);
+    if (timer != NULL)
+    {
+        RCSOFTCHECK(rcl_publish(&counter_publisher, &counter_msg, NULL));
+        ++counter_msg.data;
+    }
+}
+
 void setup()
 {
     // Initialize LED pins
@@ -81,6 +98,8 @@ void setup()
     led_response_msg.message.capacity = 25;
     led_response_msg.message.data = (char*) malloc(led_response_msg.message.capacity * sizeof(char));
     led_response_msg.message.size = 0;
+    
+    counter_msg.data = 0;
 
     // Initialize serial or WiFi transport
 #ifdef MICRO_ROS_TRANSPORT_ARDUINO_SERIAL
@@ -107,21 +126,37 @@ void setup()
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
     RCCHECK(rclc_node_init_default(&node, "led_control_node", "", &support));
 
-    //Initialize service
+    // Initialize LED control service
     RCCHECK(rclc_service_init_default(
         &led_control_service,
         &node,
         ROSIDL_GET_SRV_TYPE_SUPPORT(my_custom_led_interface, srv, MyCustomLedControl),
         "control_led"));
 
-    // Initialize executor
-    RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+    // Initialize counter publisher
+    RCCHECK(rclc_publisher_init_default(
+        &counter_publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "counter"));
+
+    // Initialize counter timer (1-second interval)
+    RCCHECK(rclc_timer_init_default2(
+        &counter_timer,
+        &support,
+        RCL_MS_TO_NS(1000),
+        counter_timer_callback,
+        true));
+
+    // Initialize executor with added timer and service
+    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
     RCCHECK(rclc_executor_add_service(
         &executor, 
         &led_control_service, 
         &led_request_msg, 
         &led_response_msg, 
         led_control_callback));
+    RCCHECK(rclc_executor_add_timer(&executor, &counter_timer));
 }
 
 void loop()

7a8c9ce895034b9027680934f96ce96b56f617fe
AgitDev-sudo <a.tunc1@student.han.nl>
2024-10-31 23:49:40 +0100

    A working example with a publisher that publishes that state of each LED and a service where you can turn onn and off with a custom message that also response with the state and a message

--- ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
+++ ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
@@ -5,7 +5,7 @@
 #include <rclc/executor.h>
 
 #include <my_custom_led_interface/srv/my_custom_led_control.h>
-#include <std_msgs/msg/int32.h>
+#include <std_msgs/msg/string.h>
 
 // Define pins for red and yellow LEDs
 #define RED_LED_PIN 3
@@ -15,29 +15,36 @@
 #define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}} 
 
-// Node, Publisher, and Service Handles
-rcl_node_t node;
-rcl_publisher_t led_status_publisher;
-rcl_publisher_t counter_publisher; // Publisher voor de counter
-rcl_service_t led_control_service;
-rcl_timer_t led_status_timer;
-rcl_timer_t counter_timer;
-rclc_executor_t executor;
-
+//Node handles
 rcl_allocator_t allocator;
 rclc_support_t support;
+rcl_node_t node;
+
+//Publisher handles
+rcl_publisher_t led_status_publisher;
+
+//Service handles
+rcl_service_t led_control_service;
+
+//Handle for Timer
+rcl_timer_t led_status_timer;
+
+//Handle for Timer, Service and Subscription (i.e. callback functions. A executor is needed for callback functions)
+rclc_executor_t executor;
 
 // Request and Response message allocations
 my_custom_led_interface__srv__MyCustomLedControl_Request led_request_msg;
 my_custom_led_interface__srv__MyCustomLedControl_Response led_response_msg;
-std_msgs__msg__Int32 counter_msg;
+
+// Publish message allocation
+std_msgs__msg__String led_pub_msg;
+const uint8_t PUB_MSG_CAPACITY = 50;
 
 // Status variables for the LEDs
 volatile bool red_led_status = false;
 volatile bool yellow_led_status = false;
-//int counter_value = 0; // Counter waarde die elke seconde wordt opgehoogd
 
-// Error handle loop, prevents for now that the esp won't reboot
+// Error handle loop, prevents that the esp won't reboot when rclc does not get initialized properly (e.g. no connection with agent)
 void error_loop()
 {
     while (1)
@@ -81,8 +88,11 @@ void counter_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
     RCLC_UNUSED(last_call_time);
     if (timer != NULL)
     {
-        RCSOFTCHECK(rcl_publish(&counter_publisher, &counter_msg, NULL));
-        ++counter_msg.data;
+        char buffer[PUB_MSG_CAPACITY];
+        sprintf(buffer, "Red LED is %s and Yellow LED is %s" , (red_led_status) ? "On" : "Off",  (yellow_led_status) ? "On" : "Off" );
+        strcpy(led_pub_msg.data.data, buffer);
+        led_pub_msg.data.size = strlen(led_pub_msg.data.data);
+        RCSOFTCHECK(rcl_publish(&led_status_publisher, &led_pub_msg, NULL));
     }
 }
 
@@ -99,7 +109,11 @@ void setup()
     led_response_msg.message.data = (char*) malloc(led_response_msg.message.capacity * sizeof(char));
     led_response_msg.message.size = 0;
     
-    counter_msg.data = 0;
+
+    led_pub_msg.data.capacity = PUB_MSG_CAPACITY;
+    led_pub_msg.data.data = (char*) malloc(led_pub_msg.data.capacity * sizeof(char));
+    led_pub_msg.data.size = 0;
+    std_msgs__msg__String__init(&led_pub_msg);
 
     // Initialize serial or WiFi transport
 #ifdef MICRO_ROS_TRANSPORT_ARDUINO_SERIAL
@@ -133,16 +147,16 @@ void setup()
         ROSIDL_GET_SRV_TYPE_SUPPORT(my_custom_led_interface, srv, MyCustomLedControl),
         "control_led"));
 
-    // Initialize counter publisher
+    // Initialize publisher
     RCCHECK(rclc_publisher_init_default(
-        &counter_publisher,
+        &led_status_publisher,
         &node,
-        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-        "counter"));
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+        "led_status"));
 
     // Initialize counter timer (1-second interval)
     RCCHECK(rclc_timer_init_default2(
-        &counter_timer,
+        &led_status_timer,
         &support,
         RCL_MS_TO_NS(1000),
         counter_timer_callback,
@@ -150,13 +164,14 @@ void setup()
 
     // Initialize executor with added timer and service
     RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+
     RCCHECK(rclc_executor_add_service(
         &executor, 
         &led_control_service, 
         &led_request_msg, 
         &led_response_msg, 
         led_control_callback));
-    RCCHECK(rclc_executor_add_timer(&executor, &counter_timer));
+    RCCHECK(rclc_executor_add_timer(&executor, &led_status_timer));
 }
 
 void loop()

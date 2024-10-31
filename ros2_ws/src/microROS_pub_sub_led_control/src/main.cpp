351af3c430c7ccddbac7bf1f9bc7824a975e3854
AgitDev-sudo <a.tunc1@student.han.nl>
2024-10-31 21:18:54 +0100

    Removed comments, now only the code of the services is visable

--- ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
+++ ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
@@ -3,8 +3,7 @@
 #include <rcl/rcl.h>
 #include <rclc/rclc.h>
 #include <rclc/executor.h>
-//#include <std_msgs/msg/string.h>
-//#include <std_msgs/msg/int32.h>
+
 #include <my_custom_led_interface/srv/my_custom_led_control.h>
 
 // Define pins for red and yellow LEDs
@@ -22,17 +21,16 @@ rcl_service_t led_control_service;
 rcl_timer_t led_status_timer;
 rclc_executor_t executor;
 
+rcl_allocator_t allocator;
+rclc_support_t support;
+
 // Request and Response message allocations
 my_custom_led_interface__srv__MyCustomLedControl_Request led_request_msg;
 my_custom_led_interface__srv__MyCustomLedControl_Response led_response_msg;
 
-//publish msgs
-//std_msgs__msg__String led_pub_msg;
-//std_msgs__msg__Int32 msg;
-
 // Status variables for the LEDs
-bool red_led_status = false;
-bool yellow_led_status = false;
+volatile bool red_led_status = false;
+volatile bool yellow_led_status = false;
 
 // Error handle loop, prefents for now that the esp won't reboot
 void error_loop()
@@ -72,21 +70,6 @@ void led_control_callback(const void *req, void *res)
     response->message.size = strlen(response->message.data);
 }
 
-// Callback for the LED status publisher timer
-// void led_status_publish_callback(rcl_timer_t *timer, int64_t last_call_time)
-// {
-//     if (timer != NULL)
-//     {
-//         RCSOFTCHECK(rcl_publish(&led_status_publisher, &msg, NULL));
-//         msg.data++;
-//         // char buffer[50];
-//         // sprintf(buffer, "Red LED is %s and Yellow LED is %s" , (red_led_status) ? "On" : "Off",  (yellow_led_status) ? "On" : "Off" );
-//         // strcpy(led_pub_msg.data.data, buffer);
-//         // led_pub_msg.data.size = strlen(led_pub_msg.data.data);
-//         // RCSOFTCHECK(rcl_publish(&led_status_publisher, &led_pub_msg, NULL));
-//     }
-// }
-
 void setup()
 {
     // Initialize LED pins
@@ -99,11 +82,6 @@ void setup()
     led_response_msg.message.capacity = 25;
     led_response_msg.message.data = (char*) malloc(led_response_msg.message.capacity * sizeof(char));
     led_response_msg.message.size = 0;
-
-    // led_pub_msg.data.capacity = 50;
-    // led_pub_msg.data.data = (char*) malloc(led_pub_msg.data.capacity * sizeof(char));
-    // led_pub_msg.data.size = 0;
-    // msg.data = 0;
 
     // Initialize serial or WiFi transport
 #ifdef MICRO_ROS_TRANSPORT_ARDUINO_SERIAL
@@ -125,23 +103,10 @@ void setup()
 #endif
 
     // Initialize micro-ROS
-    rcl_allocator_t allocator = rcl_get_default_allocator();
-    rclc_support_t support;
+    allocator = rcl_get_default_allocator();
+    
     RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
     RCCHECK(rclc_node_init_default(&node, "led_control_node", "", &support));
-
-    // Initialize publisher
-    // RCCHECK(rclc_publisher_init_default(
-    //     &led_status_publisher,
-    //     &node,
-    //     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
-    //     "led_status"));
-
-    // RCCHECK(rclc_publisher_init_default(
-    //     &led_status_publisher,
-    //     &node,
-    //     ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-    //     "led_status"));
 
     //Initialize service
     RCCHECK(rclc_service_init_default(
@@ -150,17 +115,7 @@ void setup()
         ROSIDL_GET_SRV_TYPE_SUPPORT(my_custom_led_interface, srv, MyCustomLedControl),
         "control_led"));
 
-    // Initialize timer
-    // const unsigned int timer_period_ms = 1000;
-    // RCCHECK(rclc_timer_init_default2(
-    //     &led_status_timer,
-    //     &support,
-    //     RCL_MS_TO_NS(timer_period_ms),
-    //     led_status_publish_callback,
-    //     true));
-
     // Initialize executor
-    //RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
     RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
     RCCHECK(rclc_executor_add_service(
         &executor, 
@@ -168,11 +123,10 @@ void setup()
         &led_request_msg, 
         &led_response_msg, 
         led_control_callback));
-    //RCCHECK(rclc_executor_add_timer(&executor, &led_status_timer));
 }
 
 void loop()
 {
-    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
     delay(100);
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }

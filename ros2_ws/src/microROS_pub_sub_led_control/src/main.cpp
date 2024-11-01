23edb076bd67988da662faedc90a11b21ac536aa
AgitDev-sudo <a.tunc1@student.han.nl>
2024-11-01 10:45:45 +0100

    Working example of a ros node for the esp32 which has a publisher that published the state en brightnes of LED, a subscriber where we can configure the brightnes which will be applied for all LED, and a service where we can turn on and off a LED control with our custom message. Also a lot of comments is added in the source code for detailed explanation

--- ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
+++ ros2_ws/src/microROS_pub_sub_led_control/src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <my_custom_led_interface/srv/my_custom_led_control.h>
 #include <std_msgs/msg/string.h>
+#include <std_msgs/msg/u_int8.h>
 
 // Define pins for red and yellow LEDs
 #define RED_LED_PIN 3
@@ -26,6 +27,9 @@ rcl_publisher_t led_status_publisher;
 //Service handles
 rcl_service_t led_control_service;
 
+// Subscriber handles
+rcl_subscription_t brightness_subscriber; // Toegevoegd voor de nieuwe subscriber
+
 //Handle for Timer
 rcl_timer_t led_status_timer;
 
@@ -38,7 +42,10 @@ my_custom_led_interface__srv__MyCustomLedControl_Response led_response_msg;
 
 // Publish message allocation
 std_msgs__msg__String led_pub_msg;
-const uint8_t PUB_MSG_CAPACITY = 50;
+const uint8_t PUB_MSG_CAPACITY = (71+4+4+3); //calculation for max message size
+
+// LED brightness control message
+std_msgs__msg__UInt8 brightness_msg;
 
 // Status variables for the LEDs
 volatile bool red_led_status = false;
@@ -60,16 +67,19 @@ void led_control_callback(const void *req, void *res)
     my_custom_led_interface__srv__MyCustomLedControl_Response *response = (my_custom_led_interface__srv__MyCustomLedControl_Response *)res;
 
     response->success = true;
+    const uint8_t brightness = (request->led_on) ? brightness_msg.data : 0;
 
     if (request->led_color == 1)
     {
-        digitalWrite(RED_LED_PIN, request->led_on);
+        analogWrite(RED_LED_PIN, brightness);
+
         red_led_status = request->led_on;
         strcpy(response->message.data, (red_led_status) ? "Red LED turned on" : "Red LED turned off");
     }
     else if (request->led_color == 2)
     {
-        digitalWrite(YELLOW_LED_PIN, request->led_on);
+        analogWrite(YELLOW_LED_PIN, brightness);
+
         yellow_led_status = request->led_on;
         strcpy(response->message.data, (yellow_led_status) ? "Yellow LED turned on" : "Yellow LED turned off");
     }
@@ -82,6 +92,23 @@ void led_control_callback(const void *req, void *res)
     response->message.size = strlen(response->message.data);
 }
 
+// Callback function for LED brightness subscriber
+void brightness_callback(const void *msg_in)
+{
+    const std_msgs__msg__UInt8 *msg = (const std_msgs__msg__UInt8 *)msg_in;
+    const uint8_t brightness = msg->data;
+    
+    if(red_led_status)
+    {
+        analogWrite(RED_LED_PIN, brightness);
+    }
+
+    if(yellow_led_status)
+    {
+        analogWrite(YELLOW_LED_PIN, brightness); 
+    }
+}
+
 // Callback function for counter timer
 void counter_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 {
@@ -89,7 +116,7 @@ void counter_timer_callback(rcl_timer_t *timer, int64_t last_call_time)
     if (timer != NULL)
     {
         char buffer[PUB_MSG_CAPACITY];
-        sprintf(buffer, "Red LED is %s and Yellow LED is %s" , (red_led_status) ? "On" : "Off",  (yellow_led_status) ? "On" : "Off" );
+        sprintf(buffer, "Red LED is %s and Yellow LED is %s. Brightness value for all LED is %d" , (red_led_status) ? "On" : "Off",  (yellow_led_status) ? "On" : "Off", brightness_msg.data);
         strcpy(led_pub_msg.data.data, buffer);
         led_pub_msg.data.size = strlen(led_pub_msg.data.data);
         RCSOFTCHECK(rcl_publish(&led_status_publisher, &led_pub_msg, NULL));
@@ -104,17 +131,23 @@ void setup()
     digitalWrite(RED_LED_PIN, LOW);
     digitalWrite(YELLOW_LED_PIN, LOW);
 
-    //init msg sizes
+    //Init msg sizes
     led_response_msg.message.capacity = 25;
     led_response_msg.message.data = (char*) malloc(led_response_msg.message.capacity * sizeof(char));
+    //Alternatively this could be used instead of above:
+    //led_response_msg.message.data = (char*) allocator.allocate(led_response_msg.message.capacity * sizeof(char), allocator.state);
+    //By default allocator uses malloc as implementation (see rcl_get_default_allocator()), custom allocator(s) could also be made for specific platforms. 
+    //In this example we just use malloc directly, because allacotor is not initialized at this stage of the code.
     led_response_msg.message.size = 0;
     
 
     led_pub_msg.data.capacity = PUB_MSG_CAPACITY;
     led_pub_msg.data.data = (char*) malloc(led_pub_msg.data.capacity * sizeof(char));
     led_pub_msg.data.size = 0;
-    std_msgs__msg__String__init(&led_pub_msg);
 
+    brightness_msg.data = 255; // full brightness for all LEDS as initial value.
+
+    
     // Initialize serial or WiFi transport
 #ifdef MICRO_ROS_TRANSPORT_ARDUINO_SERIAL
     Serial.begin(SERIAL_BAUDRATE);
@@ -154,6 +187,13 @@ void setup()
         ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
         "led_status"));
 
+   // Initialize brightness subscriber
+    RCCHECK(rclc_subscription_init_default(
+        &brightness_subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt8),
+        "led_brightness"));
+
     // Initialize counter timer (1-second interval)
     RCCHECK(rclc_timer_init_default2(
         &led_status_timer,
@@ -162,9 +202,12 @@ void setup()
         counter_timer_callback,
         true));
 
-    // Initialize executor with added timer and service
-    RCCHECK(rclc_executor_init(&executor, &support.context, 2, &allocator));
+    // Initialize executor with timer and service and subcriber 
+    // Third argument is total number_of_handles, which means the total number of subscriptions, timers, services, clients and guard conditions. Do not include the number of nodes and publishers!!!
+    RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator)); //number 3 is used because we have 1 timer, 1 service and 1 subscriber. 
 
+    //Order of using rlcl_executor_add_* function matters, this decides order of execution for service, pub, sub, timer.
+    //In this example first Service, then timer for publishing and then subscription.
     RCCHECK(rclc_executor_add_service(
         &executor, 
         &led_control_service, 
@@ -172,10 +215,15 @@ void setup()
         &led_response_msg, 
         led_control_callback));
     RCCHECK(rclc_executor_add_timer(&executor, &led_status_timer));
+    RCCHECK(rclc_executor_add_subscription(&executor, &brightness_subscriber, &brightness_msg, &brightness_callback, ON_NEW_DATA));
+
 }
 
 void loop()
 {
-    delay(100);
-    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
+    RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100))); //timeout of 100 ms (which means this function blocks for 100 ms)
+    
+    //Alternatively this could be used instead of above:
+    //rclc_executor_spin(&executor); //This function runs forever without coming back
+    //Since we run this code in arduino framework, using the 'rclc_executor_spin_some' function in the loop function seems more appropriate.
 }

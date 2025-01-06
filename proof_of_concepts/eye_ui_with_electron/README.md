6a792533522c8dd1a63248d4c2a6380958d0b97d
AgitDev-sudo <a.tunc1@student.han.nl>
2025-01-06 12:14:37 +0100

    basic hello world Electron App

---
+++ proof_of_concepts/eye_ui_with_electron/README.md
@@ -0,0 +1,11 @@
+# POC eyes with Electron
+
+## installation
+
+```bash
+mkdr project_dr #Voor mij
+cd project_dr #Voor mij
+npm init -y # Voor mij initialiseer project met npm
+npm install --save-dev electron #Install electron 
+
+```
---
+++ proof_of_concepts/eye_ui_with_electron/main.js
@@ -0,0 +1,13 @@
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile('index.html');
+});

8a52d5910e420cc583cc4983cd55c5560f4a56a2
AgitDev-sudo <a.tunc1@student.han.nl>
2025-02-21 15:43:36 +0100

    Refector camera_lld to ros2 style guide and added namespace

--- ros2_ws/src/camera_lld/src/main.cpp
+++ ros2_ws/src/camera_lld/src/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<CameraLLD>());
+    rclcpp::spin(std::make_shared<camera_lld::CameraLLD>());
     rclcpp::shutdown();
     return 0;
 }
